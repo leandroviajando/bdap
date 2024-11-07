b54fdc978a2d12907210721fb097cf3b7e8cdc31
leandroviajando <65241663+leandroviajando@users.noreply.github.com>
2024-11-07 21:39:16 +0100

    WIP

--- BloomFilter/src/BloomFilter.java
+++ BloomFilter/src/BloomFilter.java
@@ -1,29 +1,42 @@
 import java.nio.charset.StandardCharsets;
+import java.util.Vector;
 
+/**
+ * This class implements a bloom filter with k hash functions and m bits.
+ * The bloom filter is used to check if an element is in a set or not.
+ * The bloom filter can have false positives but no false negatives.
+ * 
+ * That is, if for at least one hash function h_i, B[h_i(e)] = 0, then the
+ * element is definitely not in the set. However, if B[h_i(e)] = 1 for all h_i,
+ * there is still a chance that the element may in fact not be in the set.
+ */
 public class BloomFilter {
-    
-    /**
-     * This class implements a bloom filter with k hash functions and m bits.
-     * The bloom filter is used to check if an element is in a set or not.
-     * The bloom filter can have false positives but no false negatives.
-     */
 
-    
-    private int logNumBits; // log of the number of bits
-    private int k; // number of hash functions
-    // TODO: Add other parameters as needed
+    /** Total number of bits in the bloom filter */
+    private int numBits;
+
+    /** Number of hash functions */
+    private int k;
+
+    /** Vector to hold the bits */
+    private Vector<Integer> bits;
 
     /**
-     * Constructs a BloomFilter object with the specified number of bits and hash functions.
+     * Constructs a BloomFilter object with the specified number of bits and hash
+     * functions.
      *
      * @param logNumBits the log of the number of bits in the bloom filter
-     * @param k the number of hash functions to be used
+     * @param k          the number of hash functions to be used
      */
     public BloomFilter(int logNumBits, int k) {
-        
-        this.logNumBits = logNumBits;
+        this.numBits = 1 << logNumBits; // Bitwise left shift: 2^logNumBits - 1
         this.k = k;
-        // TODO: Complete this method
+
+        System.out.printf("%s = %s, %s = %s, %s = %s%n", "logNumBits", logNumBits, "numBits", numBits, "k", k);
+
+        this.bits = new Vector<>(numBits);
+        for (int i = 0; i < numBits; i++)
+            bits.add(0);
     }
 
     /**
@@ -32,33 +45,43 @@ public class BloomFilter {
      * @param s the element to be added
      */
     public void add(String s) {
-        // TODO: Complete this method
+        for (int i = 0; i < k; i++) {
+            int index = getIndex(s, i);
+            bits.set(index, 1);
+        }
     }
 
     /**
      * Checks if the specified element is present in the bloom filter.
      *
      * @param s the element to be checked
-     * @return true if the element is present, false otherwise
+     * @return false if any bit is not set, else true
      */
     public boolean check(String s) {
-        // TODO: Complete this method
+        for (int i = 0; i < k; i++) {
+            int index = getIndex(s, i);
+            if (bits.get(index) == 0)
+                return false;
+        }
         return true;
     }
-
 
     /**
      * Returns the index of the specified element in the bloom filter.
      *
      * @param element the element to get the index for
-     * @param seed the seed value for the hash function
+     * @param seed    the seed value for the hash function
      * @return the index of the element in the bloom filter
      */
     private int getIndex(String element, int seed) {
+        byte[] bytes = element.getBytes(StandardCharsets.UTF_8);
+        int hash = MurmurHash.hash32(bytes, bytes.length, seed);
 
-        // TODO: Complete this method
-        return -1;
+        /**
+         * Bitwise ANDs to ensure hash
+         * 1. is non-negative (by clearing the sign-bit), and
+         * 2. fits within bits vector size
+         */
+        return hash & 0x7FFFFFFF & (numBits - 1);
     }
-
-
 }
--- RadixSort/src/RadixSort.java
+++ RadixSort/src/RadixSort.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class RadixSort {
+
     /**
      * Sorts an array of strings using the Radix Sort algorithm.
      *
@@ -8,21 +9,66 @@ public class RadixSort {
      * @return the sorted array of strings
      */
     public static String[] radixSort(String[] arr) {
+        int maxLength = 0;
+        for (String str : arr)
+            if (str.length() > maxLength)
+                maxLength = str.length();
 
-        // TODO: Complete this method
+        for (int pos = maxLength - 1; pos >= 0; pos--)
+            countingSort(arr, pos);
+
         return arr;
+    }
+
+    /**
+     * A stable counting sort algorithm that sorts the strings based on the
+     * character at the given position.
+     *
+     * @param arr the array of strings to be sorted
+     * @param pos the position of the character to sort by (0 is the rightmost)
+     */
+    private static void countingSort(String[] arr, int pos) {
+        final int ASCII_RANGE = 256; // Extended ASCII
+
+        int[] count = new int[ASCII_RANGE]; // Count of occurrences of characters at given pos
+        Arrays.fill(count, 0);
+        for (String str : arr) {
+
+            char ch = '\0'; // Default character: for shorter strings
+            if (pos < str.length())
+                ch = str.charAt(pos); // Character in str at pos
+
+            count[ch]++;
+        }
+        for (int i = 1; i < ASCII_RANGE; i++)
+            count[i] += count[i - 1]; // Transform to cumulative count
+
+        String[] output = new String[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            char ch = '\0'; // Default character: for shorter strings
+            if (pos < arr[i].length())
+                ch = arr[i].charAt(pos);
+
+            output[count[ch] - 1] = arr[i];
+            count[ch]--;
+        }
+
+        // Copy the output array back to arr
+        System.arraycopy(output, 0, arr, 0, arr.length);
     }
 
     public static void main(String[] args) {
         String[] arr = { "apple", "banana", "pear", "kiwi", "avocado" };
 
-        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println("\nOriginal array: " + Arrays.toString(arr));
         String[] sorted = radixSort(arr);
         System.out.println("Sorted array: " + Arrays.toString(sorted));
 
         String[] arr2 = { "c3e", "bfr", "aAD", "Abb", "Rtr", "Ast", "ASb", "Arv", "ASf", "rr", "gtz", "tgk", "mdf",
                 "wfrf", "rfrx", "nrfr", "ffro", "srf", "ufrf", "frry" };
-        System.out.println("Original array: " + Arrays.toString(arr2));
+
+        System.out.println("\nOriginal array: " + Arrays.toString(arr2));
         String[] sorted2 = radixSort(arr2);
         System.out.println("Sorted array: " + Arrays.toString(sorted2));
     }
